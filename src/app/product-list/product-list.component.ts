056e232bbcf0e26de180c727acba29f8c493102c
traxcer <meloy30@hotmail.com>
2019-08-02 18:26:50 +0200

    4 files modified

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -13,6 +13,9 @@ export class ProductListComponent {
   share() {
     window.alert('El producto ha sido compartido!');
   }
+  onNotify() {
+    window.alert('You will be notified when the product goes on sale');
+  }
 }
 
 
